46711a2e25e8779215e8bceac31e156c322a248a
johnyjchan <chenyaojie007@sina.com>
2019-05-17 13:30:40 +0800

    Add files via upload

---
+++ Regular_expression.py
@@ -0,0 +1,92 @@
+# -*- encoding:utf-8 -*-
+"""
+    author:cyj
+    stupid regular expression implementation that only support to parse patterns involve operator '( )' , '|'
+"""
+import time
+
+PATTERN = '10(2|1)'
+TEST_CASES = ['01020101001', '111102000', '111100321111111', '2222010122103213',\
+              '222111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111112010122103213']
+
+
+def timer(func):
+    def get_runtime(*args, **kwargs):
+        start = time.clock()
+        func(*args, **kwargs)
+        end = time.clock()
+        print(func.__name__,' spend time:', end - start)
+
+    return get_runtime
+
+
+class PatternNode(object):
+    def __init__(self):
+        self.next = None
+        self.options = None
+
+    def hasNext(self):
+        return True if self.next != None else False
+
+
+class RetrievePattern(object):
+    @staticmethod
+    def compile_pattern(pattern):
+        index = 0
+        head = node = PatternNode()
+        while index < len(pattern):
+            next_node = PatternNode()
+            ascii_code = ord(pattern[index])
+            az = ascii_code >= ord('a') and ascii_code <= ord('z')
+            AZ = ascii_code >= ord('A') and ascii_code <= ord('Z')
+            zeroNine = ascii_code >= ord('0') and ascii_code <= ord('9')
+            if az or AZ or zeroNine:
+                next_node.options = [pattern[index]]
+                node.next = next_node
+                node = next_node
+            elif '(' == pattern[index]:
+                right_boundary = pattern[index:].find(')') + index
+                if -1 == right_boundary:
+                    raise Exception("could not parse PATTERN")
+                else:
+                    next_node.options = pattern[index+1:right_boundary].split('|')
+                index = right_boundary
+                node.next = next_node
+                node = next_node
+            else:
+                raise Exception("could not parse PATTERN")
+            index += 1
+        return head.next
+
+
+class AutoMachine(object):
+    def load_test_case(self, test_case, head):
+        for index in range(len(test_case)):
+            if self.state_transition(test_case, index, head):
+                return True
+        return False
+
+    def state_transition(self, test_case, index, head):
+        while index < len(test_case):
+            if test_case[index] in head.options and not head.hasNext():
+                return True
+            elif test_case[index] in head.options and head.hasNext():
+                index = index+1
+                head = head.next
+            elif not test_case[index] in head.options:
+                return False
+            else:
+                raise Exception("autoMachine timeout")
+
+
+RetrievePattern = RetrievePattern()
+AutoMachine = AutoMachine()
+
+
+@timer
+def stupid_regular_expression(PATTERN, TEST_CASES):
+    linkList_pattern = RetrievePattern.compile_pattern(PATTERN)
+    for test_case in TEST_CASES:
+        print(AutoMachine.load_test_case(test_case, linkList_pattern))
+
+stupid_regular_expression(PATTERN, TEST_CASES)
